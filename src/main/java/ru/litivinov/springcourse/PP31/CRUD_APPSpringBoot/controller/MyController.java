7f2cc73479f87e1a6de74fcf11ccc7740f5d0480
LitvinovAlexandr <sanlitv56@gmail.com>
2024-05-15 17:29:26 +0500

    edit Controller

--- src/main/java/ru/litivinov/springcourse/PP31/CRUD_APPSpringBoot/controller/MyController.java
+++ src/main/java/ru/litivinov/springcourse/PP31/CRUD_APPSpringBoot/controller/MyController.java
@@ -4,45 +4,40 @@ package ru.litivinov.springcourse.PP31.CRUD_APPSpringBoot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.litivinov.springcourse.PP31.CRUD_APPSpringBoot.entity.User;
 import ru.litivinov.springcourse.PP31.CRUD_APPSpringBoot.service.UserService;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class MyController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/")
+    @GetMapping
     public String showAllUsers(Model model) {
-
         List<User> allUsers = userService.getAllUsers();
         model.addAttribute("allUs", allUsers);
         return "all-users";
     }
 
-    @RequestMapping("/addNewUser")
+    @GetMapping("/addNewUser")
     public String addNewUser(Model model) {
-
         User user = new User();
         model.addAttribute("user", user);
-
         return "user-info";
     }
 
-    @RequestMapping("/saveUser")
+    @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute("user") User user) {
         userService.saveUser(user);
         return "redirect:/";
     }
 
-    @RequestMapping("/updateInfo")
+    @GetMapping("/updateInfo")
     public String updateUser(@RequestParam("usId") int id, Model model) {
-
         User user = userService.getUser(id);
         model.addAttribute("user", user);
         return "user-info";
@@ -50,7 +45,6 @@ public class MyController {
 
     @RequestMapping("/deleteUser")
     public String deleteUser(@RequestParam("usId") int id) {
-
         userService.deleteUser(id);
         return "redirect:/";
     }
